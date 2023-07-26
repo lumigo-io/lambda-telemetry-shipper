46c324d76b48eb531de2306ad9678a02b9aa99ef
saartochner <saart@lumigo.io>
2023-07-26 10:45:22 +0300

    modify the deploy script to support local deployments

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,11 +1,16 @@
 echo "Extension layer"
+rm -rf extensions extension-python-modules extensions.zip runtime.zip python-runtime __MACOSX
 echo "-Installing the package and its dependencies"
 python -W ignore setup.py --quiet bdist_wheel  # create .egg-info
-enc_location=../common-resources/encrypted_files/credentials_production.enc
-mkdir -p ~/.aws
-echo ${KEY} | gpg --batch -d --passphrase-fd 0 ${enc_location} > ~/.aws/credentials
-unset AWS_ACCESS_KEY_ID
-unset AWS_SECRET_ACCESS_KEY
+
+if [ -n "$CI" ]; then
+  echo "- Use credentials_production"
+  enc_location=../common-resources/encrypted_files/credentials_production.enc
+  mkdir -p ~/.aws
+  echo ${KEY} | gpg --batch -d --passphrase-fd 0 ${enc_location} > ~/.aws/credentials
+  unset AWS_ACCESS_KEY_ID
+  unset AWS_SECRET_ACCESS_KEY
+fi
 pushd src > /dev/null || exit
 
 echo "-create directories to zip"
@@ -34,17 +39,21 @@ for region in "${regions[@]}"; do
     version=$(aws lambda publish-layer-version --layer-name "${layer_name}" --description "No-code to ship your telemetry logs" --zip-file fileb://extensions.zip --region ${region} --cli-connect-timeout 6000 | jq -r '.Version')
     aws lambda add-layer-version-permission --layer-name "${layer_name}" --statement-id engineering-org --principal "*" --action lambda:GetLayerVersion --version-number ${version} --region ${region} > /dev/null
     echo "published layer version: ${version} in region: ${region}"
-    echo "arn:aws:lambda:${region}:723663554526:layer:${layer_name}:${version}"
+    if [ -n "$CI" ]; then
+      echo "arn:aws:lambda:${region}:723663554526:layer:${layer_name}:${version}"
+    fi
 done
 rm -rf extensions extension-python-modules extensions.zip runtime.zip python-runtime __MACOSX
 popd > /dev/null || exit
 
 
-echo "-update README"
-sed -i "s/\(arn:aws:lambda:<region>:114300393969:layer:${layer_name}:\)[0-9]*/\1${version}/" README.md
-git add README.md || true
-git commit -m"update layer ARN in README [skip ci]" || true
-git push origin main || true
+if [ -n "$CI" ]; then
+  echo "-update README"
+  sed -i "s/\(arn:aws:lambda:<region>:114300393969:layer:${layer_name}:\)[0-9]*/\1${version}/" README.md
+  git add README.md || true
+  git commit -m"update layer ARN in README [skip ci]" || true
+  git push origin main || true
+fi
 
 echo
 echo "Done! Latest version: ${version}"
